dab7b0c20e481556b539a528d81bd7d79a0984c0
GAVIN <GAVINBISSELL@GMAIL.COM>
2024-08-08 15:13:26 -0600

    deploment hard :(

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -6,7 +6,7 @@ import "primereact/resources/themes/md-dark-indigo/theme.css"
 import App from './App';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
-if (reocess.env.NODE_ENV === 'production') disableReactDevTools()
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
