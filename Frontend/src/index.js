da40b2dd901962d5c9269b0a27e8f60f8968a4bf
GAVIN <GAVINBISSELL@GMAIL.COM>
2024-08-08 15:06:39 -0600

    remoed nodemon

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,6 +4,9 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primereact/resources/themes/md-dark-indigo/theme.css"
 import App from './App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (reocess.env.NODE_ENV === 'production') disableReactDevTools()
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
