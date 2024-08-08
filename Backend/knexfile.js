c0fd469e1d629b5bf4b107aed60d39a46b2fb123
GAVIN <GAVINBISSELL@GMAIL.COM>
2024-08-08 15:30:38 -0600

    deploment hard :(2

--- Backend/knexfile.js
+++ Backend/knexfile.js
@@ -4,9 +4,38 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
   development: {
     client: 'pg',
-    connection: 'postgresql://postgres:postgres@localhost:5432/crud_inventory'
+    connection: {
+      host: 'localhost',
+      user: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      port: 5432
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
   },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT || 5432,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
 };
